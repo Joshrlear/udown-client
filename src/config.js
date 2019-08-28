dfe62720c07889a7bee5569b3903b9c3a4bdef3d
Josh Lear <joshrlear@gmail.com>
2019-08-27 19:56:21 -0700

    .

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_ENDPOINT: process.env.API_ENDPOINT || "http://localhost:8000/",
     GOOGLE_MAPS_API_ENDPOINT: process.env.REACT_APP_GOOGLE_MAPS_API_ENDPOINT,
     GOOGLE_MAPS_API_KEY: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 }
